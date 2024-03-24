e6a767937b1164c7d2e3133e255b693d9d19d4b6
Siwa12100 <jean.marcillac12@gmail.com>
2024-03-24 21:58:57 +0100

    premiere version finie

---
+++ siteValorium/composants/blocImageTexteV1/BlocImageTexteV1.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace siteValorium.composants.blocImageTexteV1
+{
+    public partial class BlocImageTexteV1
+    {
+        [Parameter]
+        public String cheminImage {  get; set; }
+
+        [Parameter]
+        public String titre {  get; set; }
+
+        [Parameter]
+        public MarkupString texte { get; set; }
+
+    }
+}
---
+++ siteValorium/composants/carteOpaque/CarteOpaque.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace siteValorium.composants.carteOpaque
+{
+    public partial class CarteOpaque
+    {
+
+        [Parameter]
+        public String titre {  get; set; }
+
+        [Parameter]
+        public String contenu { get; set; }
+    }
+}
