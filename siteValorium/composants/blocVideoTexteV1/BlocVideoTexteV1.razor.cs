6bce10b2606f75baf1dfe7ed5db39d8bd721ef76
Siwa12100 <jean.marcillac12@gmail.com>
2024-03-26 09:30:31 +0100

    galère avec le /

---
+++ siteValorium/composants/blocVideoTexteV1/BlocVideoTexteV1.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace siteValorium.composants.blocVideoTexteV1
+{
+    public partial class BlocVideoTexteV1
+    {
+        [Parameter]
+        public String cheminVideo { get; set; }
+
+        [Parameter]
+        public String titre { get; set; }
+
+        [Parameter]
+        public MarkupString texte { get; set; }
+
+    }
+}
