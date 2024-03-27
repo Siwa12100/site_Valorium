962a08af19b0b8a725d0a2a9b2274ac1cd0362b7
Siwa12100 <jean.marcillac12@gmail.com>
2024-03-27 12:36:35 +0100

    avancée de la conception du site

---
+++ siteValorium/composants/blocTexteSurGradient/BlocTexteSurGradient.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace siteValorium.composants.blocTexteSurGradient
+{
+    public partial class BlocTexteSurGradient
+    {
+        [Parameter]
+        public String titre {  get; set; }
+
+        [Parameter]
+        public MarkupString contenu { get; set; }
+    }
+}
--- siteValorium/composants/navbar/Navbar.razor.cs
+++ siteValorium/composants/navbar/Navbar.razor.cs
@@ -1,7 +1,18 @@
-﻿namespace siteValorium.composants.navbar
+﻿using Microsoft.AspNetCore.Components;
+
+namespace siteValorium.composants.navbar
 {
     public partial class Navbar
     {
+        private ElementReference elementRef;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await elementRef.FocusAsync();
+            }
+        }
 
     }
 }
