e155a4884e5c419af217c90750d4ee7d94ba51f0
Siwa12100 <jean.marcillac12@gmail.com>
2024-03-27 09:05:21 +0100

    maj tour

---
+++ siteValorium/composants/blocImageTexteV2/BlocImageTexteV2.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using siteValorium.composants.blocImageTexteV1;
+
+namespace siteValorium.composants.blocImageTexteV2
+{
+    public partial class BlocImageTexteV2
+    {
+        [Parameter]
+        public String cheminImage { get; set; }
+
+        [Parameter]
+        public String titre { get; set; }
+
+        [Parameter]
+        public MarkupString texte { get; set; }
+
+    }
+}
